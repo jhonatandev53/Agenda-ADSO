922f1216faa0383cbed90634cb01482bd46bfbe2
Jhonatan Dev <londonojhonatan57@gmail.com>
2026-07-30 16:08:18 -0500

    Actividad 4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import "./App.css"
-import ContactoCard from "./components/ContactoCard"; 
+import { useState } from "react";
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+import FormularioContacto from "./components/FormularioContacto";
 
 export default function App() {
-  const contactos = [
+  const [contactos, setContactos] = useState([
     {
       id: 1,
       nombre: "Carolina Pérez",
@@ -10,60 +12,37 @@ export default function App() {
       correo: "carolina@sena.edu.co",
       etiqueta: "Compañera",
     },
-    {
-      id: 2,
-      nombre: "Juan Díaz",
-      telefono: "301 987 6543",
-      correo: "juan@sena.edu.co",
-      etiqueta: "Instructor",
-    },
-    {
-      id: 3,
-      nombre: "Luisa Martínez",
-      telefono: "320 555 7788",
-      correo: "luisa@sena.edu.co",
-      etiqueta: "Cliente",
-    },
+  ]);
 
-    {
-      id: 4,
-      nombre: "Salome Patño",
-      telefono: "301 670 6655",
-      correo: "salito@miamor.com",
-      etiqueta: "Instructor",
-    },
+  // Agregar
+  const agregarContacto = (nuevo) => {
+    setContactos((prev) => [...prev, { id: Date.now(), ...nuevo }]);
+  };
 
-    {
-      
-      id: 5,
-      nombre: "Bibiana Muñoz",
-      telefono: "316 372 2828",
-      correo: "bibi.popo91@gmail.com",
-      etiqueta: "Cliente",
-    },
-
-  ];
+  // Eliminar
+  const eliminarContacto = (id) => {
+    setContactos((prev) => prev.filter((c) => c.id !== id));
+  };
 
   return (
     <main className="app-container">
-      <h1 className="app-title">Agenda ADSO 📒</h1>
+      <h1 className="app-title">Agenda ADSO v2</h1>
 
-      <p className="app-subtitle">Contactos guardados</p>
+      <FormularioContacto onAgregar={agregarContacto} />
 
-      {contactos.map((c) => (
-        <ContactoCard
-          key={c.id}           
-          nombre={c.nombre}    
-          telefono={c.telefono} 
-          correo={c.correo}    
-          etiqueta={c.etiqueta}
-        />
-      ))}
-
-      <p className="app-nota">
-        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
-      </p>
+      <section className="lista-contactos">
+        {contactos.map((c) => (
+          <ContactoCard
+            key={c.id}
+            id={c.id}
+            nombre={c.nombre}
+            telefono={c.telefono}
+            correo={c.correo}
+            etiqueta={c.etiqueta}
+            onDelete={eliminarContacto}
+          />
+        ))}
+      </section>
     </main>
   );
 }
-
